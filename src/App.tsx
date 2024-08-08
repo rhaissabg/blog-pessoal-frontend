b5fb958965ae737beb0261bd3932d35eabf8a797
Rhaíssa Lima <rhaissasbgl@gmail.com>
2024-08-08 13:48:34 -0300

    conexão com backend

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,26 @@ import Navbar from "./components/Navbar/Navbar"
 import Home from "./pages/Home/Home"
 import Login from "./pages/Login/Login.tsx"
 import Footer from "./components/Footer/Footer"
+import Cadastro from "./pages/Cadastro/Cadastro.tsx"
+import { AuthProvider } from "./contexts/AuthContext.tsx"
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Navbar/>
-        <div className="min-h-[80vh]">
-          <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/login" element={<Login/>} />
-            <Route path="/home" element={<Home/>} />
-          </Routes>
-        </div>
-        <Footer/>
-      </BrowserRouter>
+    <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+        </AuthProvider>
     </>
   )
 }
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 import { FacebookLogo, InstagramLogo } from "@phosphor-icons/react"
 import { LinkedinLogo } from "@phosphor-icons/react/dist/ssr"
 
